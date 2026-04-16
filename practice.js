8ec8615caaa52f0dc2ff6289b826652d0f1ac425
Chanams123 <chanamspielman@gmail.com>
2026-04-16 16:21:09 -0400

    Your message

--- practice.js
+++ practice.js
@@ -1,3 +1,25 @@
-https://www.omdbapi.com/?s=fast&apikey=da22de3a
+/* https://www.omdbapi.com/?s=fast&apikey=da22de3a */
 
+const searchBtn = document.getElementById('searchBtn')
+const searchInput = document.getElementById('searchInput')
+const results = document.getElementById('results')
 
+searchBtn.addEventListener('click', async function() {
+    const query = searchInput.value;
+    const response = await fetch (
+        https://www.omdbapi.com/?s=fast&apikey=da22de3a
+    );
+    const data = await response.json();
+    const filtered = data.filter(user =>
+        user.name.toLowerCase().includes(query.toLowerCase))
+);
+results.innerHTML = ""
+filtered.forEach(function(user) {
+    results.innerHTML +=
+    <div>
+        <h3>${user.name}</h3>
+        <p>${user.email}</p>
+    </div>
+    ;
+});
+});
