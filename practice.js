36b08d3446cde94ec53b1c78741ac1c18b99d315
Chanams123 <chanamspielman@gmail.com>
2026-04-16 20:15:22 -0400

    Your message

--- practice.js
+++ practice.js
@@ -1,25 +1,25 @@
-/* https://www.omdbapi.com/?s=fast&apikey=da22de3a */
-
 const searchBtn = document.getElementById('searchBtn')
 const searchInput = document.getElementById('searchInput')
 const results = document.getElementById('results')
 
 searchBtn.addEventListener('click', async function() {
     const query = searchInput.value;
-    const response = await fetch (
-        https://www.omdbapi.com/?s=fast&apikey=da22de3a
-    );
-    const data = await response.json();
-    const filtered = data.filter(user =>
-        user.name.toLowerCase().includes(query.toLowerCase))
-);
-results.innerHTML = ""
-filtered.forEach(function(user) {
-    results.innerHTML +=
-    <div>
-        <h3>${user.name}</h3>
-        <p>${user.email}</p>
-    </div>
-    ;
-});
+    if (query) {
+        const response = await fetch(
+            `https://www.omdbapi.com/?s=${query}&apikey=da22de3a`
+        );
+        const data = await response.json();
+        if (data.Search) {
+            results.innerHTML = "";
+            data.Search.forEach(function(movie) {
+                results.innerHTML +=
+                `<div>
+                    <h3>${movie.Title}</h3>
+                    <p>${movie.Year}</p>
+                </div>`;
+            });
+        } else {
+            results.innerHTML = "<p>No results found.</p>";
+        }
+    }
 });
